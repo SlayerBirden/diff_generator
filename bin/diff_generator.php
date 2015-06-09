c7e2280788583a502f6d3f2320b982442e34079d
SlayerBirden <olegkulik1985@gmail.com>
2015-06-09 16:10:33 +0300

    update bin script to work with different hierarchy

--- bin/diff_generator.php
+++ bin/diff_generator.php
@@ -1,6 +1,23 @@
 <?php
 
-require dirname(__DIR__) . '/vendor/autoload.php';
+$included = false;
+
+foreach (array(__DIR__ . '/../../autoload.php', __DIR__ . '/../vendor/autoload.php', __DIR__ . '/vendor/autoload.php') as $file) {
+    if (file_exists($file)) {
+        include $file;
+        $included = true;
+        break;
+    }
+}
+
+if (!$included) {
+    fwrite(STDERR,
+        'You need to set up the project dependencies using the following commands:' . PHP_EOL .
+        'wget http://getcomposer.org/composer.phar' . PHP_EOL .
+        'php composer.phar install' . PHP_EOL
+    );
+    die(1);
+}
 
 $app = new \Symfony\Component\Console\Application();
 $app->add(new \SlayerBirden\Command\GenerateCreateCommand());
